1fcb37729a4d036b7923231f7a1d1917351d1ff2
MatheusZuchiBalbinot <matheusbalbinotzuchi@gmail.com>
2023-09-29 16:36:34 -0300

    Mudando os nomes

---
+++ frontend/src/components/ElementComponents/FriendCard/FriendCard.jsx
@@ -0,0 +1,16 @@
+import Avatar from '../Avatar/Avatar';
+import styles from './FriendCard.module.css';
+
+function FriendCard({ friendRequest, avatar, nickName, descriptXcaracters }) {
+  return (
+    <div className={styles.friendRequest__user__div}>
+      {avatar && <Avatar userInfo={friendRequest} size={'small'} />}
+      <div className={styles.friendRequest__user__userInfo}>
+        <h2>{nickName}</h2>
+        <p>{descriptXcaracters}</p>
+      </div>
+    </div>
+  );
+}
+
+export default FriendCard;
--- frontend/src/components/HomeComponents/AsideRightComponent/RightUserMenu/UserRightComponent.jsx
+++ frontend/src/components/HomeComponents/AsideRightComponent/RightUserMenu/UserRightComponent.jsx
@@ -1,55 +1,53 @@
-import { useContext, useState } from 'react'
-
-import { userContext} from '../../../../context/userContext'
-
-import {BsFillPersonFill} from 'react-icons/bs'
-import {FiLogOut} from 'react-icons/fi'
-
-import ShowAsideFriend from '../RightTabs/FriendTab'
-import ShowAsideTweets from '../RightTabs/TweetsTab'
-
-import styles from './UserRightComponent.module.css'
+import React, { useState } from 'react';
+import ShowAsideRequests from '../RightTabs/RequestsTab';
+import ShowAsideTweets from '../RightTabs/TweetsTab';
+import ShowAsideFriends from '../RightTabs/FriendsTab';
+import styles from './UserRightComponent.module.css';
 
 export default function UserRightComponents() {
+    const [selectedTab, setSelectedTab] = useState('tweets');
 
-    const {isLogged, setIsLogged} = useContext(userContext)
+    const handleTabClick = (tab) => {
+        setSelectedTab(tab);
+    };
 
-    const {nickName, passwordsMatch} = isLogged
-
-    const [showFriendComponent, setShowFriendComponent] = useState(false)
-    const [showTweetsComponent, setShowTweetsComponent] = useState(true)
-
-    const changeAsideComponent = (selected) => {
-        if(selected == "friends") {
-            setShowFriendComponent(true)
-            setShowTweetsComponent(false)
-            document.getElementById("friends").style.borderBottom = '3px solid green'
-            document.getElementById("tweets").style.borderBottom = '3px solid white'
-        }
-        else {
-            setShowFriendComponent(false)
-            setShowTweetsComponent(true)
-            document.getElementById("tweets").style.borderBottom = '3px solid green'
-            document.getElementById("friends").style.borderBottom = '3px solid white'
-        }
-    }
+    const getTabStyle = (tab) => {
+        return selectedTab === tab ? '3px solid green' : '3px solid white';
+    };
 
     return (
         <>
             <div className={styles.asideSelect}>
-                <button className={styles.selectAsideButtons} id="friends" onClick={() => changeAsideComponent("friends")}>Amigos</button>
-                <button className={styles.selectAsideButtonsBegin} id="tweets" onClick={() => changeAsideComponent("tweets")}>Tweets</button>
+                <button
+                    className={styles.selectAsideButtons}
+                    id="friends"
+                    style={{ borderBottom: getTabStyle('friends') }}
+                    onClick={() => handleTabClick('friends')}
+                >
+                    Amigos
+                </button>
+                <button
+                    className={styles.selectAsideButtons}
+                    id="requests"
+                    style={{ borderBottom: getTabStyle('requests') }}
+                    onClick={() => handleTabClick('requests')}
+                >
+                    Solicitações
+                </button>
+                <button
+                    className={styles.selectAsideButtonsBegin}
+                    id="tweets"
+                    style={{ borderBottom: getTabStyle('tweets') }}
+                    onClick={() => handleTabClick('tweets')}
+                >
+                    Tweets
+                </button>
             </div>
-            {showFriendComponent && (
-                <div className={styles.asideSelectedItem}>
-                    <ShowAsideFriend />
-                </div>
-            )}
-            {showTweetsComponent && (
-                <div className={styles.asideSelectedItem}>
-                    <ShowAsideTweets />
-                </div>
-            )}
+            <div className={styles.asideSelectedItem}>
+                {selectedTab === 'friends' && <ShowAsideFriends />}
+                {selectedTab === 'requests' && <ShowAsideRequests />}
+                {selectedTab === 'tweets' && <ShowAsideTweets />}
+            </div>
         </>
-    )
+    );
 }
